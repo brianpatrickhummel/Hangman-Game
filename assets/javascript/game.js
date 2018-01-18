54bae4777c0c30dbc43643a6a844a8cc255ea230
brianpatrickhummel <frankgucci@mac.com>
2018-01-18 13:08:05 -0800

    revising the click start logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -78,132 +78,130 @@ var songs = [
   "assets/music/uncleacid.mp3"
 ];
 
-window.onkeyup = function(event) {
-  $("#blink").click(() => {
-    $("#blink2").remove(); //removes the Blink() affected Press Space Bar to Play Again message from Game End of previous session
-    $("#blink2").remove();
+$(document).on("click", "#blink", () => {
+  $("#blink").remove();
+  $("#blink2").remove();
+  $("#guessBanner").html("");
+  $("#lettersG").html("");
+  $("#lettersR").html("");
+  $("#guessedLetters").html("");
+  var remainingGuesses = 8;
+  var lettersGuessed = [];
+  var answerArray = [];
+  var computerWord = potentialWords[Math.floor(Math.random() * potentialWords.length)];
+  var lettersRemaining = computerWord.length - (computerWord.split(" ").length - 1); //will subtract any spaces from lettersRemaining
+  var c = potentialWords.indexOf(computerWord); //index referencing index of computerWord to be used for selecting matched photos/songs from arrays
+  var imageIndex = imageLinks[c]; //variable for loadPicture() pull from image array with same index as computerWord
+  var loadSongs = songs[c]; //variable for loadSong() to pull from songs array with same index as computerWord
+
+  function loadPicture() {
+    //function to load image URLs from imageLinks array when game ends
+    $("#bgImage").css({ "background-image": "url(" + imageIndex + ")" });
+  }
+
+  function loadSong() {
+    var audioElement = document.createElement("audio");
+    audioElement.setAttribute("id", "music");
+    audioElement.setAttribute("src", loadSongs);
+    document.body.appendChild(audioElement);
+    audioElement.play();
+  }
+
+  function loadGif() {
+    //randomly selects background image from array when game is initialized
+    var backgroundImage = backgroundGifs[Math.floor(Math.random() * backgroundGifs.length)];
+    $("#bgImage").css({ "background-image": "url(" + backgroundImage + ")" });
+  }
+
+  function endGame() {
     $("#guessBanner").html("");
     $("#lettersG").html("");
     $("#lettersR").html("");
     $("#guessedLetters").html("");
-    var remainingGuesses = 8;
-    var lettersGuessed = [];
-    var answerArray = [];
-    var computerWord = potentialWords[Math.floor(Math.random() * potentialWords.length)];
-    var lettersRemaining = computerWord.length - (computerWord.split(" ").length - 1); //will subtract any spaces from lettersRemaining
-    var c = potentialWords.indexOf(computerWord); //index referencing index of computerWord to be used for selecting matched photos/songs from arrays
-    var imageIndex = imageLinks[c]; //variable for loadPicture() pull from image array with same index as computerWord
-    var loadSongs = songs[c]; //variable for loadSong() to pull from songs array with same index as computerWord
+    $("<div>")
+      .attr("id", "blink")
+      .css({ color: "blue" })
+      .html("<p id='blink'>Click<a class='keys'> Here </a> To Begin</p>")
+      .prependTo("#game");
+    $("#blink").blink();
+  }
 
-    function loadPicture() {
-      //function to load image URLs from imageLinks array when game ends
-      $("#bgImage").css({ "background-image": "url(" + imageIndex + ")" });
+  var html =
+    "<p id='guessBanner'>GUESS A LETTER</p>" + //basic HTML for game load screen
+    "<p id= 'win' >WINS: " +
+    wins +
+    "</p>" +
+    "<p id= 'lose' >LOSSES: " +
+    losses +
+    "</p>";
+  document.querySelector("#game").innerHTML = html; //loading basic HTML screen
+  loadGif();
+  for (var h = 0; h < computerWord.length; h++) {
+    if (computerWord.charAt(h) == " ") {
+      //load placeholder underscores into array
+      answerArray.push(" ");
+    } else {
+      answerArray.push("_");
+    }
+  }
+  var a = answerArray.join("&nbsp;");
+  $("#answer").html(a); //  display placeholder via HTML
+
+  document.onkeypress = function(event) {
+    //capture keypress
+    var x = event.which;
+    var userGuess = "";
+
+    if (x >= 65 && x <= 90) {
+      var userGuess = String.fromCharCode(x).toLowerCase();
+    } else if (x >= 97 && x <= 122) {
+      var userGuess = String.fromCharCode(x);
+    } else if (x == 32) {
+      return false;
+    } else {
+      alert("Please enter an alphabetical letter");
     }
 
-    function loadSong() {
-      var audioElement = document.createElement("audio");
-      audioElement.setAttribute("id", "music");
-      audioElement.setAttribute("src", loadSongs);
-      document.body.appendChild(audioElement);
-      audioElement.play();
-    }
-
-    function loadGif() {
-      //randomly selects background image from array when game is initialized
-      var backgroundImage = backgroundGifs[Math.floor(Math.random() * backgroundGifs.length)];
-      $("#bgImage").css({ "background-image": "url(" + backgroundImage + ")" });
-    }
-
-    function endGame() {
-      $("#guessBanner").html("");
-      $("#lettersG").html("");
-      $("#lettersR").html("");
-      $("#guessedLetters").html("");
-      $("<div>")
-        .attr("id", "blink2")
-        .css({ color: "blue" })
-        .text("Press Space Bar To Play Again")
-        .appendTo("#container");
-      $("#blink2").blink();
-    }
-
-    var html =
-      "<p id='guessBanner'>GUESS A LETTER</p>" + //basic HTML for game load screen
-      "<p id= 'win' >WINS: " +
-      wins +
-      "</p>" +
-      "<p id= 'lose' >LOSSES: " +
-      losses +
-      "</p>";
-    document.querySelector("#game").innerHTML = html; //loading basic HTML screen
-    loadGif();
-    for (var h = 0; h < computerWord.length; h++) {
-      if (computerWord.charAt(h) == " ") {
-        //load placeholder underscores into array
-        answerArray.push(" ");
+    if (lettersGuessed.indexOf(userGuess) !== -1 || answerArray.indexOf(userGuess) !== -1) {
+      alert("You have already guessed this letter.  Guess again");
+    } else if (lettersRemaining != 0 && remainingGuesses != 0) {
+      var j = computerWord.indexOf(userGuess);
+      if (j === -1) {
+        lettersGuessed.push(userGuess);
+        remainingGuesses--;
       } else {
-        answerArray.push("_");
-      }
-    }
-    var a = answerArray.join("&nbsp;");
-    $("#answer").html(a); //  display placeholder via HTML
-
-    document.onkeypress = function(event) {
-      //capture keypress
-      var x = event.which;
-      var userGuess = "";
-
-      if (x >= 65 && x <= 90) {
-        var userGuess = String.fromCharCode(x).toLowerCase();
-      } else if (x >= 97 && x <= 122) {
-        var userGuess = String.fromCharCode(x);
-      } else if (x == 32) {
-        return false;
-      } else {
-        alert("Please enter an alphabetical letter");
-      }
-
-      if (lettersGuessed.indexOf(userGuess) !== -1 || answerArray.indexOf(userGuess) !== -1) {
-        alert("You have already guessed this letter.  Guess again");
-      } else if (lettersRemaining != 0 && remainingGuesses != 0) {
-        var j = computerWord.indexOf(userGuess);
-        if (j === -1) {
-          lettersGuessed.push(userGuess);
-          remainingGuesses--;
-        } else {
-          //       ALTERNATE METHOD, ACCOUNTING FOR MULTIPLE LETTERS AT DIFF INDEXES																	//     var blanks = computerWord.split("").map(function (){return "_"})
-          for (var k = 0; k < computerWord.length; k++) {
-            //	   var index = computerWord.indexOf(userGuess)
-            if (userGuess === computerWord[k]) {
-              //	   while (index > -1){
-              answerArray[k] = userGuess; //			blanks[index] = userGuess;
-              lettersRemaining--; //			var index = computerWord.indexOf(userGuess, index + 1); }
-            }
+        //       ALTERNATE METHOD, ACCOUNTING FOR MULTIPLE LETTERS AT DIFF INDEXES																	//     var blanks = computerWord.split("").map(function (){return "_"})
+        for (var k = 0; k < computerWord.length; k++) {
+          //	   var index = computerWord.indexOf(userGuess)
+          if (userGuess === computerWord[k]) {
+            //	   while (index > -1){
+            answerArray[k] = userGuess; //			blanks[index] = userGuess;
+            lettersRemaining--; //			var index = computerWord.indexOf(userGuess, index + 1); }
           }
         }
-
-        $("#lettersG").html("<p>Guesses Remaining: " + remainingGuesses + " </p>");
-        $("#lettersR").html("<p>Number of unguessed letters: " + lettersRemaining + "</p>");
-        $("#guessedLetters").html("<p>Incorrect letters guessed: " + lettersGuessed + "</p>");
-        document.getElementById("answer").innerHTML = answerArray.join("&nbsp;");
-
-        if (lettersRemaining === 0) {
-          $("#music").remove(); //removes the current <audio> element thereby stopping any currently playing song
-          loadPicture();
-          loadSong();
-          wins++;
-          $("#win").html("Wins: " + wins);
-          $("#answer").html("You have won the game");
-          endGame();
-        }
-        if (remainingGuesses === 0) {
-          loadPicture();
-          losses++;
-          $("#lose").html("Losses: " + losses);
-          $("#answer").html("You have lost the game");
-          endGame();
-        }
       }
-    };
-  });
-};
+
+      $("#lettersG").html("<p>Guesses Remaining: " + remainingGuesses + " </p>");
+      $("#lettersR").html("<p>Number of unguessed letters: " + lettersRemaining + "</p>");
+      $("#guessedLetters").html("<p>Incorrect letters guessed: " + lettersGuessed + "</p>");
+      document.getElementById("answer").innerHTML = answerArray.join("&nbsp;");
+
+      if (lettersRemaining === 0) {
+        $("#music").remove(); //removes the current <audio> element thereby stopping any currently playing song
+        loadPicture();
+        loadSong();
+        wins++;
+        $("#win").html("Wins: " + wins);
+        $("#answer").html("You have won the game");
+        endGame();
+      }
+      if (remainingGuesses === 0) {
+        loadPicture();
+        losses++;
+        $("#lose").html("Losses: " + losses);
+        $("#answer").html("You have lost the game");
+        endGame();
+      }
+    }
+  };
+});
